0a2974a567671862be8ea36f9756723aa286a424
ValeriaSockenPortela <vale00sp@hotmail.com>
2024-08-16 10:18:59 -0600

    tercera entrega
    Los rangos de colores ya funcionan

--- detector.py
+++ detector.py
@@ -1,41 +1,35 @@
 import cv2
 import numpy as np
+from time import sleep
 
-limonFase1Arriba = np.array([52, 136, 59])
-limonFase1Abajo = np.array([49, 132, 55])
+limonFase1Arriba = np.array([56, 172, 85])
+limonFase1Abajo = np.array([34, 149, 64])
 
-limonFase2Arriba = np.array([51, 202, 117])
-limonFase2Abajo = np.array([48, 185, 112])
+limonFase2Arriba = np.array([55, 219, 146])
+limonFase2Abajo = np.array([34, 197, 126])
 
-limonFase3Arriba = np.array([43, 187, 142])
-limonFase3Abajo = np.array([40, 181, 135])
+limonFase3Arriba = np.array([46, 220, 167])
+limonFase3Abajo = np.array([29, 199, 146])
 
-limonFase4Arriba = np.array([34, 180, 164])
-limonFase4Abajo = np.array([32, 167, 160])
+limonFase4Arriba = np.array([40, 206, 193])
+limonFase4Abajo = np.array([20, 185, 173])
 
-# Factor de escala: 1 cm = 50 píxeles (ajusta según tu calibración)
+limonFase5Arriba = np.array([37, 53, 93])
+limonFase5Abajo = np.array([10, 32, 52])
+
+# Factor de escala: 1 cm = 50 píxeles
 scale_factor = 50.0
-
-# Crear el detector SIFT
-sift = cv2.SIFT_create()
-
-# Cargar la imagen de referencia
-reference_image_path = 'C:/Users/Socken/Documents/GitHub/detectorDeLimones-/limon.jpg'  # Reemplaza con la ruta de la imagen
-reference_image = cv2.imread(reference_image_path, cv2.IMREAD_GRAYSCALE)
-reference_keypoints, reference_descriptors = sift.detectAndCompute(reference_image, None)
+#ip de la camara
+ip = 'rtsp://192.168.1.103:554/cam/realmonitor?channel=1&subtype=0&authbasic=YWRtaW46QXJ0dXJpdDAu'
 
 # Iniciar la captura de video desde la cámara
-cap = cv2.VideoCapture(0)  # Cambié de 1 a 0 para usar la cámara predeterminada
+cap = cv2.VideoCapture(0)  # Cámara predeterminada
 if not cap.isOpened():
     print("Error: No se puede abrir la cámara")
     exit()
 
-# Crear un objeto de coincidencia de características
-bf = cv2.BFMatcher(cv2.NORM_L2, crossCheck=True)
-
 # Crear una ventana redimensionable
 cv2.namedWindow('Frame Original', cv2.WINDOW_NORMAL)
-cv2.namedWindow('SIFT Matches', cv2.WINDOW_NORMAL)
 
 while True:
     # Captura frame por frame
@@ -44,9 +38,8 @@ while True:
         print("No se puede recibir frame (el stream se ha terminado?). Saliendo ...")
         break
 
-    # Convertir el frame a escala de grises
+    # Convertir el frame a escala de grises y a colores HSV
     gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
-    #Convertir el frame a colores hsv
     hsv_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2HSV)
 
     # Aplicar un filtro de suavizado para reducir el ruido
@@ -66,9 +59,9 @@ while True:
         # Convertir el tamaño del contorno de píxeles a centímetros
         area_cm2 = area_pixels / (scale_factor ** 2)
         perimeter_cm = perimeter_pixels / scale_factor
-        #print(area_cm2)
+
         # Dibujar solo si el área es mayor a 20 cm²
-        if area_cm2 > 2.0:     #<-------------------------------------------------------------------------------------------------- AQUI
+        if area_cm2 > 2.0:
             # Dibujar el contorno en la imagen original
             cv2.drawContours(frame, [contour], -1, (0, 255, 0), 2)
 
@@ -83,35 +76,29 @@ while True:
 
             # Imprimir el área y el perímetro en centímetros
             cv2.putText(frame, f'Area: {area_cm2:.2f} cm^2', (cX, cY - 20), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
-            cv2.putText(frame, f'Perimeter: {perimeter_cm:.2f} cm', (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+            #cv2.putText(frame, f'Perimeter: {perimeter_cm:.2f} cm', (cX, cY), cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 0, 0), 2)
+
+            # Crear una máscara del contorno para calcular el color promedio
             mask = np.zeros_like(gray_frame)
             cv2.drawContours(mask, [contour], -1, 255, -1)
             mean_color = cv2.mean(hsv_frame, mask=mask)[:3]
-            #print(mean_color)
-            if limonFase1Abajo[0] <= mean_color[0] and limonFase1Abajo[1] <= mean_color[1] and limonFase1Abajo[2] <= mean_color[2]:
+            print(mean_color)
+            sleep(1)
+            # Clasificación según el color promedio en el espacio HSV
+            if limonFase1Abajo[0] <= mean_color[0] <= limonFase1Arriba[0] and limonFase1Abajo[1] <= mean_color[1] <= limonFase1Arriba[1] and limonFase1Abajo[2] <= mean_color[2] <= limonFase1Arriba[2]:
                 print("Limon de fase 1")
+            elif limonFase2Abajo[0] <= mean_color[0] <= limonFase2Arriba[0] and limonFase2Abajo[1] <= mean_color[1] <= limonFase2Arriba[1] and limonFase2Abajo[2] <= mean_color[2] <= limonFase2Arriba[2]:
+                print("Limon de fase 2")
+            elif limonFase3Abajo[0] <= mean_color[0] <= limonFase3Arriba[0] and limonFase3Abajo[1] <= mean_color[1] <= limonFase3Arriba[1] and limonFase3Abajo[2] <= mean_color[2] <= limonFase3Arriba[2]:
+                print("Limon de fase 3")
+            elif limonFase4Abajo[0] <= mean_color[0] <= limonFase4Arriba[0] and limonFase4Abajo[1] <= mean_color[1] <= limonFase4Arriba[1] and limonFase4Abajo[2] <= mean_color[2] <= limonFase4Arriba[2]:
+                print("Limon de fase 4")
+            elif limonFase5Abajo[0] <= mean_color[0] <= limonFase5Arriba[0] and limonFase5Abajo[1] <= mean_color[1] <= limonFase5Arriba[1] and limonFase5Abajo[2] <=mean_color[2] <= limonFase1Arriba[2]:
+                print("Limon de fase 5")
             else:
                 print("No coincide")
-            
 
-
-    # Detectar puntos clave y calcular descriptores en el cuadro actual para SIFT
-    keypoints, descriptors = sift.detectAndCompute(gray_frame, None)
-
-    # Encontrar los emparejamientos entre los descriptores de la imagen de referencia y los del cuadro actual
-    if descriptors is not None and len(descriptors) > 0:
-        matches = bf.match(reference_descriptors, descriptors)
-        matches = sorted(matches, key=lambda x: x.distance)
-
-        # Dibujar los primeros 10 emparejamientos
-        frame_with_matches = cv2.drawMatches(reference_image, reference_keypoints, frame, keypoints, matches[:10], None, flags=cv2.DrawMatchesFlags_NOT_DRAW_SINGLE_POINTS)
-
-        # Mostrar el cuadro con los emparejamientos dibujados
-        cv2.imshow('SIFT Matches', frame_with_matches)
-    else:
-        cv2.imshow('SIFT Matches', frame)  # Mostrar el frame original si no hay emparejamientos
-
-    # Mostrar el frame original con todos los contornos y tamaños
+    # Mostrar el frame original con los contornos
     cv2.imshow('Frame Original', frame)
 
     # Salimos del bucle con la tecla 'a'
